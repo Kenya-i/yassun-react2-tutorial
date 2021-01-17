c266aee98cd8ca11c5071723f9ca22c87fd7388b
Kenya Ishizuka <ken8116@icloud.com>
2021-01-17 22:05:50 +0900

    function2

--- src/List.js
+++ src/List.js
@@ -1,8 +1,20 @@
-export const List = ({ title }) => {
+const LANGUAGES = [
+  "javascript",
+  "C++",
+  "Ruby",
+  "Java",
+  "PHP",
+  "Go"
+]
+
+export const List = () => {
   return (
     <div>
-      <h4>{ title }</h4>
-      <div>リストです</div>
+      {
+        LANGUAGES.map((lang, index) => {
+          return <div key={ index }>{ lang }</div>
+        })
+      }
     </div>
   )
 }
